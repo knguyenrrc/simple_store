04d29ca3c5495a0f511830ebc52c4a9e087a55e2
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-10-09 14:53:49 -0500

    Updated db to read from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
-676.times do
-  product = Product.create(title: Faker::Book.title, price: Faker::Commerce.price, stock_quantity: Faker::Number.number)
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+filename = Rails.root.join("db/products.csv")
+
+csv_data = File.read(filename)
+products = CSV.parse(csv_data, headers:true, encoding: "utf-8")
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+  if category && category.valid?
+    product = category.products.create(
+      title:          p["name"],
+      price:          p["price"],
+      description:    p["description"],
+      stock_quantity: p["stock quantity"]
+    )
+  else
+    puts "invalid data."
+  end
 end
